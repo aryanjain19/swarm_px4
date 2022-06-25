51a5ad6b9615d88fffe189dea8885069798419dd
aryanjain19 <writetoaryan19@gmail.com>
2022-06-25 16:55:53 +0530

    Add list waypoint publisher to drone_0

--- five_drone/src/hover_0.cpp
+++ five_drone/src/hover_0.cpp
@@ -6,9 +6,14 @@
 #include <five_drone/Data.h>
 #include <std_srvs/SetBool.h>
 #include <std_msgs/Float64.h>
+#include <mavros_msgs/Waypoint.h>
+#include <mavros_msgs/WaypointPush.h>
+#include <mavros_msgs/CommandCode.h>
 
 mavros_msgs::State current_state;
 geometry_msgs::PoseStamped pose;
+mavros_msgs::WaypointPush wp_push_srv; // List of Waypoints
+mavros_msgs::Waypoint wp;
 const int drone = 0;
 int initial_x = 0, initial_y = 0, initial_z = 2;
 bool flag=false;
@@ -35,6 +40,52 @@ bool callback(std_srvs::SetBool::Request &req,std_srvs::SetBool::Response &res)
         pose.pose.position.x = initial_x;
         pose.pose.position.y = initial_y;
         pose.pose.position.z = initial_z+1;
+
+        
+        // WP 0
+        wp.frame          = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
+        wp.command        = mavros_msgs::CommandCode::NAV_TAKEOFF;
+        wp.is_current     = true;
+        wp.autocontinue   = true;
+        wp.x_lat          = 47.3978206;
+        wp.y_long         = 8.543987;
+        wp.z_alt          = 545.26191412;
+        wp_push_srv.request.waypoints.push_back(wp);
+        // WP 1
+        wp.frame          = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
+        wp.command        = mavros_msgs::CommandCode::NAV_LOITER_TIME;
+        wp.is_current     = false;
+        wp.autocontinue   = true;
+        wp.x_lat          = 49.3962527;
+        wp.y_long         = 8.5467917;
+        wp.z_alt          = 545.26191412;
+        wp.param1			= 10;
+        wp.param3			= 2;
+        wp.param4			= 1;
+        wp_push_srv.request.waypoints.push_back(wp);
+        
+        // WP 2
+        wp.frame          = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
+        wp.command        = mavros_msgs::CommandCode::NAV_WAYPOINT;
+        wp.is_current     = false;
+        wp.autocontinue   = true;
+        wp.x_lat          = 47.3977783;
+        wp.y_long         = 10.547906;
+        wp.z_alt          = 545.26191412;
+        wp_push_srv.request.waypoints.push_back(wp);
+
+        // WP 3
+        wp.frame          = mavros_msgs::Waypoint::FRAME_MISSION;
+        wp.command        = mavros_msgs::CommandCode::NAV_RETURN_TO_LAUNCH;
+        wp.is_current     = false;
+        wp.autocontinue   = true;
+        wp.x_lat          = 47.3977783;
+        wp.y_long         = 8.547906;
+        wp.z_alt          = 538.26191412;
+        wp_push_srv.request.waypoints.push_back(wp);
+
+        flag = true;
+        
     }
     else
     {
@@ -48,6 +99,8 @@ bool callback(std_srvs::SetBool::Request &req,std_srvs::SetBool::Response &res)
         pose.pose.position.x = initial_x;
         pose.pose.position.y = initial_y;
         pose.pose.position.z = initial_z;
+
+        flag=false;
     }
     res.success=true;
     ROS_INFO("rosservice was called");
@@ -82,6 +135,12 @@ int main(int argc, char **argv)
     ros::Publisher pub = nh.advertise<five_drone::Data>("/leader_who",10);
     ros::Subscriber leader_sub = nh.subscribe<five_drone::Data>
             ("/leader_who", 10, leader_callback);
+
+    ros::ServiceClient wp_client = nh.serviceClient<mavros_msgs::WaypointPush>
+            ("mavros/mission/push");
+    mavros_msgs::SetMode auto_set_mode;
+    auto_set_mode.request.custom_mode = "AUTO.MISSION";
+
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
@@ -136,10 +195,25 @@ int main(int argc, char **argv)
         }
 
 
+        if(flag==false)
         local_pos_pub.publish(pose);
 
         if(leader == drone || leader == -1)
         pub.publish(msg);
+
+        if(leader == drone)
+        {
+            if (wp_client.call(wp_push_srv)) 
+            {
+                ROS_INFO("Send waypoints ok: %d", wp_push_srv.response.success);
+                if (current_state.mode != "AUTO.MISSION") {
+                    if( set_mode_client.call(auto_set_mode) &&
+                        auto_set_mode.response.mode_sent){
+                        ROS_INFO("AUTO.MISSION enabled");
+                    }
+                }
+            }
+        }
 
         ros::spinOnce();
         rate.sleep();
